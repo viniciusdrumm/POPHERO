0fcbcb2b176750596d02b7d9ddb6ca36cd8d890e
Vinicius Drumm <dev.viniciusdrumm@gmail.com>
2023-11-30 22:32:53 -0300

    adicionado js no rodape da index

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,5 +1,7 @@
-document.getElementById('newsletterForm').addEventListener('submit', function(event) {
+document.getElementById('newsletterForm').addEventListener('submit', function (event) {
     event.preventDefault();
     const email = document.getElementById('email').value;
     alert('You have subscribed to our newsletter. Your email is: ' + email);
 });
+
+alert('Seja Bem-Vindo ao POP HERO! Aqui você encontra tudo sobre o universo dos Super-Heróis.')
