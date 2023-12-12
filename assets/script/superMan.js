5a4dba904ee001c6fd82f0cf27f2aa94323260ad
Miqueias Lara <miqueias.lara@gmail.com>
2023-12-11 22:13:04 -0300

    feito api com javacsript page superman

--- assets/script/superMan.js
+++ assets/script/superMan.js
@@ -1,5 +1,26 @@
 
+const container = document.getElementById('texto')
 
-fetch('https://superheroapi.com')
-.then((response) => response.json())
-.then((response) => console.log(response))
+
+fetch('https://akabab.github.io/superhero-api/api/id/644.json')
+.then(response => response.json())
+.then(response => exibeDados(response))
+
+function exibeDados(dados) 
+{
+    let elemento = document.createElement('div')
+
+    elemento.innerHTML = `
+    <p id="texto"> Nome: ${dados.biography.fullName}</p>
+    <p id="texto"> Local de Nascimento: ${dados.biography.placeOfBirth}</p>
+    <p id="texto"> Primeira Aparição: ${dados.biography.firstAppearance}</p>
+    <p id="texto"> Conexões: ${dados.connections.groupAffiliation}</p>
+    <p id="texto"> Parentes: ${dados.connections.relatives}</p>
+   
+    
+    
+    `
+    
+    container.append(elemento)
+
+}
